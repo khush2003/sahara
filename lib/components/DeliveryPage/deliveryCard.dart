a37d0b98eb6a21b3c514f82a950682393ce007f6
Supasek Dhanabordeephat <Supasekmai@gmail.com>
2023-06-13 18:37:29 +0700

    Adding appbar, review card

--- lib/components/DeliveryPage/deliveryCard.dart
+++ lib/components/DeliveryPage/deliveryCard.dart
@@ -274,3 +274,115 @@ Widget toReceiveCard(String itemName, String duration, int usability, int price,
         ),
       );
 }
+
+Widget deliveredCard(String itemName, String duration, int usability, int price, String user, String phone, int fee, String address, DateTime arrDate){
+  return
+      Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3), 
+              )
+            ],
+           border: Border.all(color: Colors.black),
+           borderRadius: BorderRadius.circular(10),
+           
+          ),
+          child: 
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // First Row
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                
+                  children: [
+                   Container(
+                    width: 150,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      // border: Border.all(color: Colors.black),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.amberAccent,
+                      image: DecorationImage(image: NetworkImage('https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg'))
+                    ),
+                     
+                   ),
+                    Expanded(
+                      child: Container(
+                        child: Column(
+                          children: [
+                            CardRowDesc('Name', itemName),
+                            CardRowDesc('Used Duration', duration),
+                            CardRowDesc('Usability', usability.toString() + '%'),
+                            CardRowDesc('Price', "฿"+ price.toString())
+                          ],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              // Black line seperating top inf and bottom info
+              Container(
+                height: 0.5,
+                decoration: BoxDecoration(
+                  color: Colors.black
+                ),
+              )
+              ,
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                          
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text('Delivery Information', style: TextStyle(fontWeight: FontWeight.bold),)
+                      ),
+                      CardRowDesc('Name', user),
+                      CardRowDesc('Phone', phone),
+                      CardRowDesc('Delivery Fee', "฿"+ fee.toString()),
+                      CardRowDesc('Address', address),
+                      CardRowDesc('Expected Arrival Date', arrDate.toString())
+                  ],
+                ),
+              ),
+               Container(
+                height: 0.5,
+                decoration: BoxDecoration(
+                  color: Colors.black
+                ),
+              )
+              ,
+              Padding(
+                padding: const EdgeInsets.all(24.0),
+                child: Center(
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+          backgroundColor: Color.fromRGBO(255, 199, 54, 1),
+        ),
+                    onPressed: (){
+                      print('Review');
+                    },
+                   child: Padding(
+                     padding: const EdgeInsets.symmetric(
+                      horizontal: 24,
+                      vertical: 16),
+                     child: Text('Review',style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),),
+                   )),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+}
+
--- lib/views/test_james.dart
+++ lib/views/test_james.dart
@@ -1,21 +1,80 @@
 import 'package:flutter/material.dart';
 
-import '../components/deliveryCard.dart';
+import '../components/DeliveryPage/deliveryAppBar.dart';
+import '../components/DeliveryPage/deliveryCard.dart';
 
-class TestJames extends StatelessWidget {
+
+bool showInTransit = true;
+bool showToDeliver = false;
+bool showPickUp = false;
+bool showDelivered = false;
+
+// class TestJames extends StatelessWidget {
+//   const TestJames({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       // appBar: deliveryAppBar(),
+//       body: Center(
+//         child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.center,
+//         mainAxisAlignment: MainAxisAlignment.start,
+//           children: [
+//             deliveryAppBar(),
+//             if(showInTransit)
+//              inTransitCard('James', '1 Day', 85,5000,'Supasek Dhanabordeephat','0885728260',200,'123/456 Soi Sukhumvit, Khwaeng Khlong Toei, Khet Khlong Toei, Bangkok 10110, Thailand'
+// ,DateTime.now())
+
+//             else if(showToDeliver)
+//             toDeliverCard('James', '1 Day', 85,5000,'Supasek Dhanabordeephat','0885728260',200,'123/456 Soi Sukhumvit, Khwaeng Khlong Toei, Khet Khlong Toei, Bangkok 10110, Thailand'
+// ,DateTime.now())
+
+//             else if(showPickUp)
+//             toReceiveCard('James', '1 Day', 85,5000,'Supasek Dhanabordeephat','0885728260',200,'123/456 Soi Sukhumvit, Khwaeng Khlong Toei, Khet Khlong Toei, Bangkok 10110, Thailand'
+// ,DateTime.now())
+            
+//             // else if(showDelivered)
+
+
+
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+class TestJames extends StatefulWidget {
   const TestJames({super.key});
 
+  
+
+  @override
+  State<TestJames> createState() => _TestJamesState();
+}
+
+class _TestJamesState extends State<TestJames> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('James View'),),
+      // appBar: newAppBar(),
       body: Center(
         child: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            inTransitCard('James', '1 Day', 85,5000,'Supasek Dhanabordeephat','0885728260',200,'123/456 Soi Sukhumvit, Khwaeng Khlong Toei, Khet Khlong Toei, Bangkok 10110, Thailand'
+            deliveryAppBar(),
+//              inTransitCard('James', '1 Day', 85,5000,'Supasek Dhanabordeephat','0885728260',200,'123/456 Soi Sukhumvit, Khwaeng Khlong Toei, Khet Khlong Toei, Bangkok 10110, Thailand'
+// ,DateTime.now()),
+              deliveredCard('James', '1 Day', 85,5000,'Supasek Dhanabordeephat','0885728260',200,'123/456 Soi Sukhumvit, Khwaeng Khlong Toei, Khet Khlong Toei, Bangkok 10110, Thailand'
 ,DateTime.now())
+
+
+
+
+
           ],
         ),
       ),
