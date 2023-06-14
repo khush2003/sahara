0e37e4594d7f474ab6462afa0c149cecb52f78ad
JoNoteJoMaMa <93032309+JoNoteJoMaMa@users.noreply.github.com>
2023-06-14 21:21:53 +0700

    Update font awesome

--- lib/views/test_note.dart
+++ lib/views/test_note.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sahara/components/primary_button.dart';
 import 'package:sahara/components/textfield_App.dart';
 import 'package:sahara/theme/app_theme.dart';
+import 'package:sahara/utils/uni_icon.dart';
+import 'package:unicons/unicons.dart';
 
 class TestNote extends StatelessWidget {
   const TestNote({super.key});
@@ -9,32 +12,45 @@ class TestNote extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 310, 0, 0),
-                child: Center(
-                  child: PrimaryButton(
-                    text: "POST REVIEW",
-                    onPressed: () {},
+            padding: const EdgeInsets.all(8.0),
+            child: Center(
+                child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 310, 0, 0),
+                  child: Center(
+                    child: PrimaryButton(
+                      text: "POST REVIEW",
+                      onPressed: () {},
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: TextfieldAPP(
-                  hintText: "input this or that",
-                  textStyle: formFieldText(color: lonely),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0, 20, 0, 0),
+                  child: TextfieldAPP(
+                    hintText: "input this or that",
+                    textStyle: formFieldText(color: lonely),
+                  ),
                 ),
-              )
-            ],
-          ),
-        ),
-      ),
-    ));
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(text: "Go home Bye Bye", style: regularText()),
+                      const WidgetSpan(
+                        child: Icon(
+                          FontAwesomeIcons.bahtSign,
+                          size: 18,
+                        ),
+                      ),
+                      TextSpan(text: "1000", style: regularText()),
+                      TextSpan(
+                          text: "\nWOW testing\nHow are you?",
+                          style: regularText()),
+                    ],
+                  ),
+                ),
+              ]),
+            ))));
   }
 }
